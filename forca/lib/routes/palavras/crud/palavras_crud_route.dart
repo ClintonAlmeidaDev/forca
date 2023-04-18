55c03397eeaadb728fd64ea2cafd5f089d196af4
ClintonAlmeidaJs <clinton.almeida10@gmail.com>
2023-04-18 08:50:29 -0300

    rotas

---
+++ forca/lib/routes/palavras/crud/palavras_crud_route.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PalavrasCRUDRoute extends StatefulWidget {
+  const PalavrasCRUDRoute({Key? key}) : super(key: key);
+
+  State<PalavrasCRUDRoute> createState() => _PalavrasCRUDRouteState();
+}
+
+class _PalavrasCRUDRouteState extends State<PalavrasCRUDRoute> {
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
